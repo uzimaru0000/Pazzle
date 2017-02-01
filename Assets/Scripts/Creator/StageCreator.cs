d3f13795bd83dff0c9f1173d16b21d0cc10890fe
uzimaru <uzimaru0601@gmail.com>
2017-02-01 18:34:27 +0900

    ステージ制作シーンを追加

---
+++ Assets/Scripts/Creator/StageCreator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageCreator : MonoBehaviour {
+
+    public InputField widthText;
+    public InputField heightText;
+
+    List<GameObject> field;
+    int width, height;
+
+	// Use this for initialization
+	void Start () {
+        width = int.Parse(widthText.text);
+        height = int.Parse(heightText.text);
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
